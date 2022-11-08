11f4c50100772b7810d49252ee0bffbcce96b1dd
Doug Rosman <drosma@artic.edu>
2022-11-08 08:55:18 -0600

    fix

--- examples/03-ERC721/04-TokenGatedSite/main.js
+++ examples/03-ERC721/04-TokenGatedSite/main.js
@@ -102,6 +102,7 @@ async function main() {
       initContainer.style.display = "none";
       VIP.textContent = "VIP: " + signerAddress;
       partymode.style.display = "block";
+      tunez.play();
     }
   }
 
