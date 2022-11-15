4edc8e2c9fd21f9b07ca912e9742026731073202
Doug Rosman <drosma@artic.edu>
2022-11-15 11:21:10 -0600

    add comments

--- examples/contracts-only/MintSequentialNFT.sol
+++ examples/contracts-only/MintSequentialNFT.sol
@@ -5,7 +5,11 @@ import "@openzeppelin/contracts@4.8.0/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.8.0/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts@4.8.0/access/Ownable.sol";
 import "@openzeppelin/contracts@4.8.0/utils/Counters.sol";
+
+// we need the Strings utility contract to provide the Strings.toString() function used in the safeMint function
 import "@openzeppelin/contracts@4.8.0/utils/Strings.sol";
+
+
 
 contract MintSequentialNFT is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
@@ -15,6 +19,7 @@ contract MintSequentialNFT is ERC721, ERC721URIStorage, Ownable {
     constructor() ERC721("SpiralNFT", "SPIRAL") {}
 
     function _baseURI() internal pure override returns (string memory) {
+        // set the baseURI, which will be used later when returning the complete tokenURI
         return "https://dougrosman-edu.github.io/blockchain-fa22/nft-media/spirals/metadata/";
     }
 
@@ -22,6 +27,7 @@ contract MintSequentialNFT is ERC721, ERC721URIStorage, Ownable {
         _tokenIdCounter.increment();
         uint256 tokenId = _tokenIdCounter.current();
         _safeMint(to, tokenId);
+        // concatenate the tokenId of the current minted token to the baseURI.
         string memory uri = string.concat(Strings.toString(tokenId), ".json");
         _setTokenURI(tokenId, uri);
     }
