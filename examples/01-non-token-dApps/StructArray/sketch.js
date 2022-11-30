ca2a4532d7b7447e42ea723e38db2b297e64bd12
dougrosman-edu <drosma@artic.edu>
2022-11-29 22:41:44 -0600

    add stuff

--- examples/01-non-token-dApps/StructArray/sketch.js
+++ examples/01-non-token-dApps/StructArray/sketch.js
@@ -1,8 +1,35 @@
-function setup() {
-    createCanvas(400, 400);
+let pixelSize = 16;
 
+function setup() {
+  createCanvas(32 * pixelSize, 32 * pixelSize);
+  background(100);
+  noStroke();
+  generateGrid();
 }
 
-function draw() {
+function mouseClicked() {
+  //generateGrid();
 
+  fill(255, 0, 0);
+  rect(floor(mouseX) * pixelSize, floor(mouseY) * pixelSize, pixelSize, pixelSize);
+
+  console.log("ok")
+}
+
+
+function generateGrid() {
+  for (let y = 0; y < height; y += pixelSize) {
+    for (let x = 0; x < width; x += pixelSize) {
+
+      let s;
+      if (Math.random() >= 0.5) {
+        s = 255;
+      } else {
+        s = 0;
+      }
+      fill(s);
+
+      rect(x, y, pixelSize, pixelSize);
+    }
+  }
 }
